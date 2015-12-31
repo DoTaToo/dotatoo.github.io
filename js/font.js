4b1a303873a19de13c22737121cda4fb8efdc5a5
TaeMin <wwwee98@gmail.com>
2015-12-31 15:24:08 +0900

    changed font

---
+++ js/font.js
@@ -0,0 +1,16 @@
+<script src="//ajax.googleapis.com/ajax/libs/webfont/1.4.10/webfont.js"></script>
+<script type="text/javascript">
+  WebFont.load({
+ 
+    // For google fonts
+    google: {
+      families: ['Droid Sans', 'Droid Serif']
+    }
+    // For early access or custom font
+    custom: {
+        families: ['Nanum Gothic'],
+        urls: ['http://fonts.googleapis.com/earlyaccess/nanumgothic.css']
+    }
+ 
+  });
+</script>
